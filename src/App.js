114a788e60a7acfa72438b86abdb587c7cd72fc1
brunash <bobazso@yahoo.com>
2022-12-08 15:21:46 -0500

    working on routes

--- src/App.js
+++ src/App.js
@@ -7,10 +7,42 @@ import {
   Outlet,
   Navigate,
 } from "react-router-dom";
+import navBar from "./components/navbar/navBar";
+import rightBar from "./components/rightBar/rightBar";
+import leftBar from "./components/leftBar/leftBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <div>
+        <navBar />
+        <div style={{ display: "flex" }}>
+          <leftBar />
+          <Outlet />
+          <rightBar />
+        </div>
+      </div>
+    );
+  }
+
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
     {
       path: "/login",
       element: <Login />,
