efae6466808be015bea184da47d6110c5c6fd1db
brunash <bobazso@yahoo.com>
2022-12-08 15:26:29 -0500

    working on routes

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import Profile from "./pages/profile/Profile";
 
 function App() {
 
+  const currentUser = true;
+
   const Layout = () => {
     return (
       <div>
@@ -28,10 +30,20 @@ function App() {
     );
   }
 
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser){
+      return <Navigate to="/login"/>
+    }
+    return children;
+  }
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: 
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>,
       children: [
         {
           path: "/",
