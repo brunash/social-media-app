1473cb808921fe15231df049c6631214e89249d6
brunash <bobazso@yahoo.com>
2022-12-19 18:11:07 -0500

    working on landing page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import RightBar from "./components/rightBar/rightBar";
 import LeftBar from "./components/leftBar/leftBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import "./style.scss";
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
 
   const Layout = () => {
     return (
-      <div>
+      <div className="theme-dark">
         <NavBar />
         <div style={{ display: "flex" }}>
           <LeftBar />
