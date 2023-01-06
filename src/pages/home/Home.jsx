fcf57e19c45fc4173de66337344bc79817f221fd
brunash <bobazso@yahoo.com>
2023-01-06 17:08:15 -0500

    working on functionality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,15 @@ import "./Home.scss";
 import navBar from "../../components/navbar/navBar";
 import Stories from "../../components/stories/Stories";
 import Posts from "../../components/posts/Posts";
+import Share from "../../components/share/Share";
 
 
 const Home = () => {
     return (
       <div className="home">
-        <Stories/>
-        <Posts/>
+        <Stories />
+        <Share />
+        <Posts />
       </div>
     );
 }
