663a54a5329c33526a0850235aa10c306d23567d
brunash <bobazso@yahoo.com>
2023-02-02 22:09:29 -0500

    working on functionality

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -14,56 +14,62 @@ import { makeRequest } from "../../axios";
 import { useLocation } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
+import Update from "../../components/update/Update";
+import { useState } from "react";
 
 const Profile = () => {
-
+  const [openUpdate, setOpenUpdate] = useState(false);
   const { currentUser } = useContext(AuthContext);
 
-  const userId = parseInt(useLocation().pathname.split("/")[2])
+  const userId = parseInt(useLocation().pathname.split("/")[2]);
 
-  const { isLoading, error, data } = useQuery(["user"], () => 
-  makeRequest.get("/users/find/"+ userId).then((res) => {
-    return res.data;
-  })
-  )
-
-  const { isLoading: rIsLoading, data: relationshipData } = useQuery(["relationship"], () =>
-    makeRequest.get("/relationships?followedUserId=" + userId).then((res) => {
+  const { isLoading, error, data } = useQuery(["user"], () =>
+    makeRequest.get("/users/find/" + userId).then((res) => {
       return res.data;
     })
   );
 
-  console.log()
+  const { isLoading: rIsLoading, data: relationshipData } = useQuery(
+    ["relationship"],
+    () =>
+      makeRequest.get("/relationships?followedUserId=" + userId).then((res) => {
+        return res.data;
+      })
+  );
 
+  const queryClient = useQueryClient();
 
-    const queryClient = useQueryClient();
-
-    const mutation = useMutation(
-      (following) => {
-        if (following) return makeRequest.delete("/relationships?userId=" + userId);
-        return makeRequest.post("/relationships", { userId });
+  const mutation = useMutation(
+    (following) => {
+      if (following)
+        return makeRequest.delete("/relationships?userId=" + userId);
+      return makeRequest.post("/relationships", { userId });
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["relationship"]);
       },
-      {
-        onSuccess: () => {
-          // Invalidate and refetch
-          queryClient.invalidateQueries(["relationship"]);
-        },
-      }
-    );
+    }
+  );
 
-    const handleFollow = () => {
-      mutation.mutate(relationshipData.includes(currentUser.id));
-    };
+  const handleFollow = () => {
+    mutation.mutate(relationshipData.includes(currentUser.id));
+  };
 
   return (
     <div className="profile">
       {isLoading ? (
-        "Loading"
+        "loading"
       ) : (
         <>
           <div className="images">
-            <img src={data.coverPic} alt="" className="cover" />
-            <img src={data.profilePic} alt="" className="profilePic" />
+            <img src={"/upload/" + data.coverPic} alt="" className="cover" />
+            <img
+              src={"/upload/" + data.profilePic}
+              alt=""
+              className="profilePic"
+            />
           </div>
           <div className="profileContainer">
             <div className="uInfo">
@@ -96,8 +102,10 @@ const Profile = () => {
                     <span>{data.website}</span>
                   </div>
                 </div>
-                {rIsLoading ? "Loading" : userId === currentUser.id ? (
-                  <button>Update</button>
+                {rIsLoading ? (
+                  "loading"
+                ) : userId === currentUser.id ? (
+                  <button onClick={() => setOpenUpdate(true)}>update</button>
                 ) : (
                   <button onClick={handleFollow}>
                     {relationshipData.includes(currentUser.id)
@@ -115,6 +123,7 @@ const Profile = () => {
           </div>
         </>
       )}
+      {openUpdate && <Update setOpenUpdate={setOpenUpdate} user={data} />}
     </div>
   );
 };
